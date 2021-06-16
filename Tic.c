6888f3be802de78c5cd92c01421e58a9bb75c1e2
BasilBosnjak <basil.bosnjak@gmail.com>
2021-06-16 14:11:27 +0200

    Setting positions, players and moves

--- Tic.c
+++ Tic.c
@@ -19,5 +19,57 @@ int main() {
         printf("-+-+-");
         printf("%c|%c|%c", pos[6], pos[7], pos[8]);
 
+        if (count % 2 == 0) {
+            sign = 'X';
+            player = 1;
+        } else {
+            sign = 'O';
+            player = 2;
+        }
+
+        printf ("Move for player %d (1-9) ", player );
+        scanf("%d", &index);
+
+        if(index<1 || index>9) {
+            printf("Invalid move. Allowed fields are from 1 to 9.\n");
+            continue;
+        }
+        
+        if (pos[index-1] == 'X' || pos[index=1] == 'O') {
+            printf ("Position is already occupied\n");
+            continue;
+        }
+
+        pos[index-1]=sign;
+        count++;
+
+        for(i=0; i<9; i++) {
+            if (i%3 == 0) {
+                flag = 0;
+            if (pos[i] == sign) {
+                flag++;
+            if (flag == 3) {
+                winner = 1;
+                win(player, winner, pos);
+            }
+
+            flag = 0;   
+
+            for(i=0; i<3; i++) {
+                for(k=1; k <= 1 + 6; k+=3) {
+                    if(pos[k]==sign)
+                        flag++;
+                }
+
+                if (flag == 3) {
+                    winner = 1;
+                    win(player, winner, pos);
+            }
+            }
+            }
+            
+            
+        }
+
     }
 }
